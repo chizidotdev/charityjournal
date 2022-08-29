ea5689666e65913479716dcf51086acf02181f32
chizidotdev <chizivwokoma@gmail.com>
2022-08-29 20:09:49 +0100

    careers page init

---
+++ src/pages/careers.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Careers = () => {
+  return (
+    <div>
+      <section className='container'>
+        <h1 className='heading-1'>Find Charity Jobs for you.</h1>
+      </section>
+    </div>
+  );
+};
+
+export default Careers;
